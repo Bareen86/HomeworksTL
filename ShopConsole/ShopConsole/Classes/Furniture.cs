2c17a137fd5f652dc38fe892e2f3a4fff67fddc0
Egor <79940875+Bareen86@users.noreply.github.com>
2023-04-07 21:40:33 +0300

    add SecondHW

---
+++ ShopConsole/ShopConsole/Classes/Furniture.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopConsole.Classes
+{
+    public class Furniture : GoodInfo
+    {
+        public string Size { get; set; }
+        public string Material { get; set; }
+    }
+}
